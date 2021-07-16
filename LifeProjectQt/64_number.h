9458759485e17b0d7cd640fad5af4372ce92d8be
FGF098 <bazhenov.095@gmail.com>
2021-07-16 21:07:20 +0300

    small fix

--- LifeProjectQt/64_number.h
+++ LifeProjectQt/64_number.h
@@ -11,15 +11,15 @@ namespace Numbers
 		Number64(int number);
 		Number64(unsigned number);
 
-		Number64& operator= (const int number);
-		Number64& operator= (const unsigned number);
+		Number64& operator= (const int& number);
+		Number64& operator= (const unsigned& number);
 		Number64& operator= (const Number64& number);
 
 		Number64 operator+ (const Number64& second) const;
 		Number64 operator- (const Number64& second) const;
 
-		Number64 operator% (const int modulo) const;
-		Number64 operator% (const unsigned modulo) const;
+		Number64 operator% (const int& modulo) const;
+		Number64 operator% (const unsigned& modulo) const;
 		Number64 operator% (const Number64& modulo) const;
 
 		bool operator== (const Number64& second) const;
